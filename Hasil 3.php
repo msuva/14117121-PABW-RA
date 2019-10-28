9166bf3ae39be60d6588b8993b1e0a63ffa5fac2
msuva <50061032+msuva@users.noreply.github.com>
2019-10-28 11:36:59 +0700

    praktikum

---
+++ Hasil 3.php
@@ -0,0 +1,13 @@
+<html>
+<body>
+
+<?php
+function faktorial($angka){
+    if ($angka > 1) {
+        return $angka * faktorial($angka-1);
+    }elseif ($angka == 1) {
+        return $angka;
+    }
+}
+echo "Hasil faktorial adalah ", faktorial($_POST['bilangan']);
+?>
---
+++ Nomor 3.php
@@ -0,0 +1,10 @@
+<html>
+<body>
+
+<form method="POST" action="nomor3hasil.php">
+Masukkan Angka <input type="number" name="bilangan"></input>
+<input type="submit">
+</form>
+
+</body>
+</html>
